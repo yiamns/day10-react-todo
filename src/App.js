925ee6f26551d64b88b810386e8c7a78a32abe61
ethan <ethan.yang@oocl.com>
2025-09-12 14:55:01 +0800

    style: Update Todo item styling and modify second todo to be marked as done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import {createContext, useContext, useReducer} from "react";
 
 export const initState = [
     {id: 1, text: "the first todo", done: false},
-    {id: 2, text: "the second todo", done: false},
+    {id: 2, text: "the second todo", done: true},
 ];
 export const TodoContext = createContext()
 
 function TodoItem(props) {
-    return <div>
-        { props.todo.text }
+    return <div className={"todo-item"}>
+        <span className={
+            props.todo.done ? "todo-done" : ""
+        }>
+            { props.todo.text }
+        </span>
     </div>;
 }
 
