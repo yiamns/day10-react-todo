053d726368019ba750f99ab746d9c36204cec2c1
ethan <ethan.yang@oocl.com>
2025-09-12 15:05:19 +0800

    feat: Implement todo completion functionality and update reducer

--- src/App.js
+++ src/App.js
@@ -8,13 +8,44 @@ export const initState = [
 export const TodoContext = createContext()
 
 function TodoItem(props) {
+    const {state, dispatch} = useContext(TodoContext);
+
+    function makeAsDone() {
+        dispatch({
+            type: 'DONE',
+            payload: {id: props.todo.id}
+        })
+    }
+
     return <div className={"todo-item"}>
         <span className={
-            props.todo.done ? "todo-done" : ""
-        }>
+            props.todo.done ? "todo-done" : ""}
+            onClick={makeAsDone}
+        >
             { props.todo.text }
         </span>
     </div>;
+}
+
+export function todoReducer(state, action) {
+    switch (action.type) {
+        case "TOGGLE_TODO":
+            /// copy
+            const newState = [...state];
+            const id = action.payload.id;
+            return newState.map((value) => {
+                if (value.id === id) {
+                    return { id,
+                        text: value.text,
+                        done: !value.done
+                    };
+                }
+
+                return value
+            })
+        default:
+            return state;
+    }
 }
 
 function TodoGroup() {
@@ -30,9 +61,6 @@ function TodoGroup() {
     )
 }
 
-export function todoReducer(state, action) {
-    return state
-}
 
 function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
